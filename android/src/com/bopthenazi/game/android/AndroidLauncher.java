812207d4310a8f53ba28af263943ca68bf6b41d1
Marc McDougall <marcjmcdougall@gmail.com>
2015-01-21 11:38:56 -0500

    Adding libGDX boilerplate code.

---
+++ android/src/com/bopthenazi/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bopthenazi.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bopthenazi.game.BopTheNaziGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BopTheNaziGame(), config);
+	}
+}
---
+++ desktop/src/com/bopthenazi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bopthenazi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bopthenazi.game.BopTheNaziGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BopTheNaziGame(), config);
+	}
+}

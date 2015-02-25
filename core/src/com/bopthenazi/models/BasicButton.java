e0acf01a430517c2e70b1f0518113f516ccc0398
Marc McDougall <marcjmcdougall@gmail.com>
2015-02-24 23:43:23 -0500

    Disabled click sounds temporarily as they were breaking the Android version.

--- core/src/com/bopthenazi/models/BasicButton.java
+++ core/src/com/bopthenazi/models/BasicButton.java
@@ -1,7 +1,7 @@
 package com.bopthenazi.models;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -16,6 +16,9 @@ public class BasicButton extends Actor {
 	private Texture clicked;
 	
 	private boolean isClicked;
+	
+	private Sound downClick;
+	private Sound upClick;
 	
 	public BasicButton(Texture unclicked, Texture clicked, float x, float y){
 		
@@ -38,6 +41,9 @@ public class BasicButton extends Actor {
 		this.unclicked = unclicked;
 		this.clicked = clicked;
 		
+//		this.downClick = Gdx.audio.newSound(Gdx.files.internal("sfx/click-down.wav"));
+//		this.upClick = 	Gdx.audio.newSound(Gdx.files.internal("sfx/click-up.wav"));
+		
 		this.addListener(new InputListener(){
 
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -46,9 +52,9 @@ public class BasicButton extends Actor {
 				
 				BasicButton.this.setClicked(true);
 				
-				Gdx.audio.newSound(Gdx.files.internal("sfx/click-down.wav")).play(0.25f);
-				
 				Gdx.app.log(BTNGame.TAG, "Touch down received on " + BasicButton.class);
+				
+//				downClick.play(0.25f);
 				
 				return true;
 			}
@@ -60,7 +66,7 @@ public class BasicButton extends Actor {
 				
 				BasicButton.this.setClicked(false);
 				
-				Gdx.audio.newSound(Gdx.files.internal("sfx/click-up.wav")).play(0.25f);
+//				upClick.play(0.25f);
 				
 				Gdx.app.log(BTNGame.TAG, "Touch up received on " + BasicButton.class);
 			}
